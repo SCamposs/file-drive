2126b8e95a9a9c8762edd566a9125449998d1b60
Ítalo Soares Campos <soarescmpos@gmail.com>
2024-03-09 18:05:47 -0300

    add clerk components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { SignInButton, useSession, SignOutButton, SignedIn, SignedOut} from "@clerk/nextjs";
 
 export default function Home() {
+  // const session = useSession()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button> Hello World</Button>
+      <SignedIn>
+        <SignOutButton>
+          <Button> Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal"> 
+          <Button> Sign In</Button>
+        </SignInButton>
+      </SignedOut>
     </main>
   );
 }
