bc687948edfa4952836503057666b9bac238c333
Ítalo Soares Campos <soarescmpos@gmail.com>
2024-04-02 18:18:10 -0300

    display favorite icon based on status

--- src/app/dashboard/_components/file-card.tsx
+++ src/app/dashboard/_components/file-card.tsx
@@ -22,6 +22,7 @@ import {
   ImageIcon,
   MoreVertical,
   StarIcon,
+  StarOffIcon,
   TrashIcon,
 } from 'lucide-react'
 
@@ -41,7 +42,13 @@ import { api } from '../../../../convex/_generated/api'
 import { useToast } from '@/components/ui/use-toast'
 import Image from 'next/image'
 
-function FileCardActions({ file }: { file: Doc<'files'> }) {
+function FileCardActions({
+  file,
+  isFavorited,
+}: {
+  file: Doc<'files'>
+  isFavorited: boolean
+}) {
   const deleteFile = useMutation(api.files.deleteFile)
   const toggleFavorite = useMutation(api.files.toggleFavorite)
   const { toast } = useToast()
@@ -88,8 +95,15 @@ function FileCardActions({ file }: { file: Doc<'files'> }) {
             }}
             className="flex gap-1 items-center cursor-pointer"
           >
-            <StarIcon className="w-4 h-4" />
-            Favorite
+            {isFavorited ? (
+              <div className="flex gap-1 items-center">
+                <StarOffIcon className="w-4 h-4" /> Unfavorite
+              </div>
+            ) : (
+              <div className="flex gap-1 items-center">
+                <StarIcon className="w-4 h-4" /> Favorite
+              </div>
+            )}
           </DropdownMenuItem>
           <DropdownMenuSeparator />
           <DropdownMenuItem
@@ -109,12 +123,22 @@ function getFileUrl(fileId: Id<'_storage'>): string {
   return `${process.env.NEXT_PUBLIC_CONVEX_URL}/api/storage/${fileId}`
 }
 
-export function FileCard({ file }: { file: Doc<'files'> }) {
+export function FileCard({
+  file,
+  favorites,
+}: {
+  file: Doc<'files'>
+  favorites: Doc<'favorites'>[]
+}) {
   const typeIcons = {
     image: <ImageIcon />,
     pdf: <FileTextIcon />,
     csv: <GanttChartIcon />,
   } as Record<Doc<'files'>['type'], ReactNode>
+
+  const isFavorited = favorites.some(
+    (favorites) => favorites.fileId === file._id,
+  )
 
   return (
     <Card>
@@ -124,7 +148,7 @@ export function FileCard({ file }: { file: Doc<'files'> }) {
           {file.name}
         </CardTitle>
         <div className="absolute top-2 right-2">
-          <FileCardActions file={file} />
+          <FileCardActions isFavorited={isFavorited} file={file} />
         </div>
       </CardHeader>
       <CardContent className="h-[200px] flex justify-center items-center">
--- src/app/dashboard/favorites/page.tsx
+++ src/app/dashboard/favorites/page.tsx
@@ -5,7 +5,7 @@ import { FileBrowser } from '../_components/file-browser'
 export default function FavoritesPage() {
   return (
     <div>
-      <FileBrowser title="Favorites" favorites />
+      <FileBrowser title="Favorites" favoritesOnly />
     </div>
   )
 }
