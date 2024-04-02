108fb3c77304db170ce80ae8ea21882069a04a2e
Ítalo Soares Campos <soarescmpos@gmail.com>
2024-04-02 15:39:52 -0300

    add: Search Bar

--- convex/files.ts
+++ convex/files.ts
@@ -59,7 +59,7 @@ export const createFile = mutation({
 })
 
 export const getFiles = query({
-  args: { orgId: v.string() },
+  args: { orgId: v.string(), query: v.optional(v.string()) },
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity()
 
@@ -77,10 +77,20 @@ export const getFiles = query({
       return []
     }
 
-    return ctx.db
+    const files = await ctx.db
       .query('files')
       .withIndex('by_orgId', (q) => q.eq('orgId', args.orgId))
       .collect()
+
+    const query = args.query
+
+    if (!query) {
+      return files
+    } else {
+      return files.filter((file) =>
+        file.name.toLowerCase().includes(query.toLowerCase()),
+      )
+    }
   },
 })
 
