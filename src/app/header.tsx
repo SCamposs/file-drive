2abdd6eadfffe1efdff4f49293e5fc03b41005f9
Ítalo Soares Campos <soarescmpos@gmail.com>
2024-04-03 21:33:19 -0300

    add: Footer & Landing Page

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button'
 import {
   OrganizationSwitcher,
   SignInButton,
+  SignedIn,
   SignedOut,
   UserButton,
 } from '@clerk/nextjs'
@@ -10,16 +11,18 @@ import Link from 'next/link'
 
 export function Header() {
   return (
-    <div className="border-b py-4 bg-gray-50">
+    <div className="relative z-10 border-b py-4 bg-gray-50">
       <div className="items-center container flex mx-auto justify-between">
         <Link href="/" className="flex gap-2 items-center text-xl">
-          <Image src="/logo.png" width="40" height="40" alt="file drive logo" />
+          <Image src="/logo.png" width="45" height="45" alt="file drive logo" />
           File Drive
         </Link>
 
-        <Button variant={'outline'}>
-          <Link href="/dashboard/files">Your Files</Link>
-        </Button>
+        <SignedIn>
+          <Button variant={'outline'} className="ml-8">
+            <Link href="/dashboard/files">Your Files</Link>
+          </Button>
+        </SignedIn>
 
         <div className="flex gap-2">
           <OrganizationSwitcher />
